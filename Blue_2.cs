a06a64a715d4bdbd7c7ba753ed6112d49a593dfa
ptrjbvg <m2414154@edu.misis.ru>
2025-04-22 13:21:31 +0300

    Update Blue_2.cs

--- Blue_2.cs
+++ Blue_2.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Authentication.ExtendedProtection.Configuration;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,197 +9,233 @@ namespace Lab_7
 {
     public class Blue_2
     {
-        public abstract class WaterJump 
+        public abstract class WaterJump
         {
-            private string _name;
-            private int _bank;
-            private Participant[] _participants;
+            private string name;
+            private int bank;
+            private Participant[] participants;
+            protected WaterJump(string name, int bank)
+            {
+                name = name;
+                bank = bank;
+                participants = new Participant[0];
+            }
 
-            public string Name => _name;
-            public int Bank => _bank;
-            public Participant[] Participants => _participants;
+            public string Name => name;
+
+            public int Bank => bank;
+
+            public Participant[] Participants
+            {
+                get { return participants; }
+            }
 
             public abstract double[] Prize { get; }
 
-            public WaterJump(string name, int bank)
-            {
-                _name = name;
-                _bank = bank;
-                _participants = new Participant[0];
-            }
-
             public void Add(Participant participant)
             {
-                if (_participants == null) return;
-                Participant[] newarr = new Participant[_participants.Length + 1];
-                for(int i = 0; i < _participants.Length; i++)
+                if (participants==null)
                 {
-                    newarr[i] = _participants[i];
+                    return;
                 }
-                newarr[_participants.Length] = participant;
-                _participants = newarr;
+                Participant[] newPart = new Participant[participants.Length + 1];
+                for (int i = 0; i < participants.Length; i++)
+                {
+                    newPart[i] = participants[i];
+                }
+                newPart[participants.Length] = participant;
+                participants = newPart;
             }
 
-            public void Add(Participant[] participants)
+            public void Add(Participant[] newParticipants)
             {
-                if (_participants == null || participants == null || participants.Length == 0) return;
-                foreach(Participant participant in participants)
+                foreach (var participant in newParticipants)
                 {
-                    Add(participant);
-                }
-            }
-        }
-
-        public class WaterJump3m : WaterJump
-        {
-            public WaterJump3m(string name, int bank) : base(name, bank) { }
-
-            public override double[] Prize
-            {
-                get
-                {
-                    if (this.Participants.Length <= 3 || this.Participants == null) return default(double[]);
-                    double[] prizes = new double[3];
-                    prizes[0] = 0.5 * this.Bank;
-                    prizes[1] = 0.3 * this.Bank;
-                    prizes[2] = 0.2 * this.Bank;
-                    return prizes;
-
-                }
-            }
-
-        }
-
-        public class WaterJump5m : WaterJump
-        {
-            public WaterJump5m(string name, int bank) : base(name, bank) { }
-
-
-            public override double[] Prize
-            {
-                get
-                {
-                    if (this.Participants.Length < 3 || this.Participants == null) return default(double[]);
-                    double[] prizes = new double[Math.Min(this.Participants.Length, 10)];
-
-                    prizes[0] = Bank * 0.40;
-                    prizes[1] = Bank * 0.25;
-                    prizes[2] = Bank * 0.15; 
-
-                    int halfCount = this.Participants.Length / 2;
-                    int topCount = Math.Min(Math.Max(halfCount, 3), 10); 
-
-                  
-                    double nPercent = 20.0 / topCount;
-
-                    for (int i = 3; i < topCount; i++)
-                    {
-                        prizes[i] = Bank * nPercent / 100;
-                    }
-
-                    return prizes;
+                    Add(participant); 
                 }
             }
         }
         public struct Participant
         {
+            private string name;
+            private string surname;
+            private int[,] marks; 
 
-            private string _name;
-            private string _surname;
-            private int[,] _marks;
-            private int _lastadd;
-
-
-            public string Name => _name;
-            public string Surname => _surname;
+          
+            public string Name => name;
+            public string Surname => surname;
             public int[,] Marks
             {
                 get
                 {
-                    if (_marks == null || _marks.GetLength(0) == 0 || _marks.GetLength(1) == 0) return null; 
-                    int[,] copy = new int[_marks.GetLength(0), _marks.GetLength(1)];
-                    for (int i = 0; i < copy.GetLength(0); i++)
-                    {
-                        for (int j = 0; j < copy.GetLength(1); j++)
-                        {
-                            copy[i, j] = _marks[i, j];
-                        }
-                    }
-                    return copy;
+                    if (marks == null || marks.Length == 0) return null;
+                    int[,] copyArray = new int[2, 5];
+                    Array.Copy(marks, copyArray, marks.Length);
+                    return copyArray;
                 }
             }
-
             public int TotalScore
             {
                 get
                 {
-                    if (_marks == null || _marks.GetLength(0) == 0 || _marks.GetLength(1) == 0) return 0;
-                    int sum = 0;
-                    for (int i = 0; i < 2; i++)
+                    if (marks == null || marks.Length == 0) return 0; 
+
+                    int total = 0;
+                    for (int i = 0; i <2; i++)
                     {
                         for (int j = 0; j < 5; j++)
                         {
-                            sum += _marks[i, j];
+                            total += marks[i, j];
                         }
                     }
-                    return sum;
+                    return total;
                 }
             }
+
 
             public Participant(string name, string surname)
             {
-                _name = name;
-                _surname = surname;
-                _marks = new int[2, 5];
-                _lastadd = -1;
+                this.name = name;
+                this.surname = surname;
+                this.marks = new int[2, 5];
             }
+
 
             public void Jump(int[] result)
             {
-                if (_marks == null || result == null) return;
-                if (_lastadd >= _marks.GetLength(0)) return;
-                for (int i = 0; i < 5; i++)
+                if (result == null || result.Length == 0)
                 {
-                    _marks[_lastadd + 1, i] = result[i];
-
+                    return;
                 }
-                _lastadd++;
+
+                int scoresToTake = 0;
+                if (result.Length < 5)
+                {
+                    scoresToTake = result.Length;
+                }
+                else
+                {
+                    scoresToTake = 5;
+                }
+                if (marks == null || marks.Length == 0)
+                {
+                    return;
+                }
+                for (int i = 0; i < 2; i++)
+                {
+                    bool isEmpty = true;
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (marks[i, j] != 0)
+                        {
+                            isEmpty = false;
+                            break;
+                        }
+                    }
+
+                    if (isEmpty)
+                    {
+                        for (int j = 0; j < scoresToTake; j++)
+                        {
+                            marks[i, j] = result[j];
+                        }
+                        return;
+                    }
+                }
+
+                return;
             }
+
 
             public static void Sort(Participant[] array)
             {
-                if (array == null || array.Length <= 1)
-                    return;
-
-                int n = array.Length;
-                bool swapped;
-
-                for (int i = 0; i < n - 1; i++)
+                if (array == null) return;
+                for (int i = 0; i < array.Length - 1; i++)
                 {
-                    swapped = false;
-
-                    for (int j = 0; j < n - 1 - i; j++)
+                    for (int j = 0; j < array.Length - i - 1; j++)
                     {
                         if (array[j].TotalScore < array[j + 1].TotalScore)
                         {
                             Participant temp = array[j];
                             array[j] = array[j + 1];
                             array[j + 1] = temp;
-
-                            swapped = true;
                         }
                     }
-
-                    if (!swapped)
-                        break;
                 }
             }
 
             public void Print()
             {
-                Console.WriteLine($"{_name} {_surname} - {TotalScore} ");
+                Console.WriteLine($"{Name} {Surname} {TotalScore}");
+            }
+        }
+        public class WaterJump3m : WaterJump
+        {
+            public WaterJump3m(string name, int bank)
+                : base(name, bank)
+            {
             }
 
+            public override double[] Prize
+            {
+                get
+                {
+                    if (Participants.Count() < 3 || Participants == null)
+                    {
+                        return null;
+                    }
+
+                    double firstPlacePrize = Bank * 0.5;
+                    double secondPlacePrize = Bank * 0.3;
+                    double thirdPlacePrize = Bank * 0.2;
+
+                    return new double[] { firstPlacePrize, secondPlacePrize, thirdPlacePrize };
+                }
+            }
+        }
+
+        public class WaterJump5m : WaterJump
+        {
+            public WaterJump5m(string name, int bank) :
+                base(name, bank)
+            { }
+            public override double[] Prize
+            {
+                get
+                {
+                    if (Participants.Count() < 3 || Participants == null)
+                    {
+                        return null;
+                    }
+                    Participant.Sort(Participants);
+                    int topParc = Participants.Length / 2;
+                    double[] prizes = null;
+                    if (topParc <= 3)
+                    {
+                        prizes = new double[3];
+                    }
+                    else if (topParc <= 10)
+                    {
+                        prizes = new double[topParc];
+                    }
+                    else
+                    {
+                        prizes = new double[10];
+                    }
+                    double firstPlacePrize = Bank * 0.4;
+                    double secondPlacePrize = Bank * 0.25;
+                    double thirdPlacePrize = Bank * 0.15;
+                    double othersPlacePrise = Bank * 20 / topParc / 100;
+                    prizes[0] = firstPlacePrize;
+                    prizes[1] = secondPlacePrize;
+                    prizes[2] = thirdPlacePrize;
+                    for (int i = 0; i < topParc; i++)
+                    {
+                        prizes[i] = othersPlacePrise;
+                    }
+                    return prizes;
+                }
+            }
         }
     }
 }
